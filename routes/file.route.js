4b162be6d3a89d621ffb781be1f99a675d45b97d
Romain Tello <rtello@iteatime.fr>
2019-07-09 17:03:29 +0200

    added zip file transfer and parsing

--- routes/file.route.js
+++ routes/file.route.js
@@ -3,12 +3,42 @@ const express = require('express');
 const app = express();
 const routes = express.Router();
 
+const formidable = require('formidable');
 const gtfs = require('gtfs');
-
 
 // Defined store route
 routes.route('/').post(function (req, res) {
-    console.log(req.body);
+    var form = new formidable.IncomingForm();
+    // form.uploadDir = './data';
+
+    form.on('fileBegin', function(name, file) {
+        file.path += '.zip';
+    });
+
+    form.parse(req, function(err, fields, files) {
+        filename = files.file.path;
+        console.log(filename);
+        gtfs.import(
+            {
+                "mongoUrl": "mongodb://localhost:27017/tempo",
+                "agencies": [
+                  {
+                    "agency_key": "STAC",
+                    "path": filename
+                  }
+                ]
+            }
+        )
+        .then(() => {
+            console.log('Import Successful');
+            // return mongoose.connection.close();
+        })
+        .catch(err => {
+            console.error(err);
+        });    
+    });
+
+    return;
 });
 
 module.exports = routes;
