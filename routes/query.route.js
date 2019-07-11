5eab362239607a83223c21b2ff4d567e71ff51c5
Romain Tello <rtello@iteatime.fr>
2019-07-11 10:40:33 +0200

    added timetables method

--- routes/query.route.js
+++ routes/query.route.js
@@ -18,4 +18,33 @@ routes.route('/').post(function (req, res) {
     });
 });
 
+routes.route('/timetables').get(function (req, res) {
+    const data = {};
+    gtfs.getRoutes()
+    .then(routes => {
+        const maxThreads = routes.length * 2;
+        let doneThreads = 0;
+        for (let route of routes) {
+            data[route.route_id] = [ [], [] ];
+            for (let direction of [0, 1]) {
+                gtfs.getStops({agency_key: 'STAC', route_id: route.route_id, direction_id: direction})
+                .then(stops => {
+                    const stop_indices = stops.map(stop => {return stop.stop_id});
+                    data[route.route_id][direction] = stop_indices;
+                    doneThreads++;
+                    if (doneThreads === maxThreads) {
+                        res.json(data);
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+            }
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
+
 module.exports = routes;
