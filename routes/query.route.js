44a5039caf57db907ede5a4d4bfe0b38969861df
Romain Tello <rtello@iteatime.fr>
2019-07-25 12:25:42 +0200

    updated timetables method for annotations

--- routes/query.route.js
+++ routes/query.route.js
@@ -20,11 +20,11 @@ routes.route('/').post(function (req, res) {
 
 routes.route('/timetables').post(async function (req, res) {
     const payload = req.body;
-
+    
     const agency_key = 'STAC';
     let routes = await gtfs.getRoutes();
     const data = {};
-
+    
     if (payload.hasOwnProperty('routes')) {
         const filteredRoutes = [];
         for (let route of routes) {
@@ -34,27 +34,46 @@ routes.route('/timetables').post(async function (req, res) {
         }
         routes = filteredRoutes;
     }
-
+    
     for (let route of routes) {
         let route_id = route.route_id;
         data[route_id] = [];
 
         for (let direction_id of [0, 1]) {
-            let stops = await gtfs.getStops({
+            let params = {
                 agency_key,
                 route_id,
                 direction_id,
-            });
+            };
+
+            let trips = await gtfs.getTrips(params);
+
+            let indexedTrips = {};
+            for (let trip of trips) {
+                let match = trip.trip_id.match(/^(\d+)-(\w+)-(\w+)-(\w+)-\d\d-?(\w*)$/);
+                indexedTrips[match[1]] = match.slice(2, 6);
+            }
+
+            let special = [];
+            for (let tripData of Object.values(indexedTrips)) {
+                if (tripData[3].length > 0) {
+                    if (!special.includes(tripData[3])) {
+                        special.push(tripData[3])
+                    }
+                }
+            }
+
+            let stops = await gtfs.getStops(params);
 
             const stop_indices = stops.map(stop => {return stop.stop_id});
 
+            let name = stops.length > 0 ? stops[stops.length - 1].stop_name : '' ;
+
             data[route_id][direction_id] = {
-                name: '',
+                name,
+                special,
                 stops: stop_indices,
                 times: {},
-            }
-            if (stops.length > 0 ) {
-              data[route_id][direction_id].name = stops[stops.length - 1].stop_name;
             }
 
             for (let stop_id of stop_indices) {
@@ -67,9 +86,8 @@ routes.route('/timetables').post(async function (req, res) {
 
                 let obj = {};
                 for (let time of times) {
-                    let match = time.trip_id.match(/^(\d+)-(\w+)-(\w+)-(\w+)-.+$/);
-                    let period = match[2];
-                    let id = match[4];
+                    let match = time.trip_id.match(/^(\d+)-/);
+                    let id = indexedTrips[match[1]].slice(1,3).join('-')
                     if (!obj.hasOwnProperty(id)) {
                         obj[id] = []
                     }
